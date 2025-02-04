09c88a5a787f6afc842b35ed256e0b76882b90d7
pawel-wojtkiewicz <wojtkiewicz.pp@gmail.com>
2025-02-04 15:38:38 +0100

    Add files via upload

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortalKsiazkowy.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Nazwa użytkownika jest wymagana.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Hasło jest wymagane.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortalKsiazkowy.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Nazwa użytkownika jest wymagana.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Hasło jest wymagane.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Potwierdzenie hasła jest wymagane.")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Hasła nie są zgodne.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
